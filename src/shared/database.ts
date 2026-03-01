f7542d2ddd24dacfc123a1999d0edf533d05df08
Hasani Rogers <dev@hasanirogers.me>
2026-03-01 10:45:31 -0700

    revert

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -4,17 +4,5 @@ const projectID = import.meta.env.SUPABASE_PROJECT_ID;
 const anonKey = import.meta.env.SUPABASE_ANON_KEY;
 const serviceRoleKey = import.meta.env.SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabase = createClient(
-  `https://${projectID}.supabase.co`,
-  anonKey,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-      flowType: 'pkce'
-    }
-  }
-);
-
+export const supabase = createClient(`https://${projectID}.supabase.co`, anonKey);
 export const supabaseAdmin = createClient(`https://${projectID}.supabase.co`, serviceRoleKey);
