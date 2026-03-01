d1ed420470792b728b9174d30c6dc09ad81ead33
Hasani Rogers <dev@hasanirogers.me>
2026-03-01 10:48:05 -0700

    hmmmm

--- src/shared/database.ts
+++ src/shared/database.ts
@@ -3,6 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const projectID = import.meta.env.SUPABASE_PROJECT_ID;
 const anonKey = import.meta.env.SUPABASE_ANON_KEY;
 const serviceRoleKey = import.meta.env.SUPABASE_SERVICE_ROLE_KEY;
+const options = {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: false,
+  }
+}
 
-export const supabase = createClient(`https://${projectID}.supabase.co`, anonKey);
+export const supabase = createClient(`https://${projectID}.supabase.co`, anonKey, options);
 export const supabaseAdmin = createClient(`https://${projectID}.supabase.co`, serviceRoleKey);
